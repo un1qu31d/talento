4f28de9ffb0313af19bc114f3398d89aa1623954
un1qu31d <un1qu31d@gmail.com>
2019-08-16 12:39:24 +0200

    201908161437

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+document.querySelector('[switch="component-page__collapse-side"]').addEventListener('click', e => {
+  document.querySelector('.component-page').classList.toggle('status--collapse-side');
+});
