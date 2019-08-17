f356c12a0cb45f05a37b0582f20c323976edc2c6
un1qu31d <un1qu31d@gmail.com>
2019-08-17 06:02:43 +0200

    201908170602

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
-document.querySelector('[switch="component-page__collapse-side"]').addEventListener('click', e => {
-  document.querySelector('.component-page').classList.toggle('status--collapse-side');
+document.querySelector('[switch="component-page__extended-side"]').addEventListener('click', e => {
+  document.querySelector('.component-page').classList.toggle('status--extended-side');
 });
